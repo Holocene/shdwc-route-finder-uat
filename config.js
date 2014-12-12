a6ebd37c784e93bf4ba706f97ed3f06209de80f5
Holocene <a@b.com>
2014-12-12 19:55:45 +0000

    oops

--- config.js
+++ config.js
@@ -9,7 +9,6 @@
     type: 'uat',
     redirectUri: 'http://holocene.github.io/shdwc-route-finder-uat/#/login-return',
     clientId: '4f25b205bcb84aaf9c9b5d20d3a57156',
-    devCode: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2IjowLCJkIjp7InVpZCI6IjkzNTYwMzM2MyIsImNoYXJhY3Rlck5hbWUiOiJPbGl4aWEgQ2FzdGl0YXRpcyJ9LCJpYXQiOjE0MTQzMjQ1MDN9.Tmib3A4110wI3LjmJkrY41dn6Ip0ksYRa80StXNpn7M",
 
     homeSystemId: 30005008
 
