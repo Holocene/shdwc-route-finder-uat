7c8902389a584fc42e457c370a34c34a5e07806c
Holocene <a@b.com>
2014-12-12 19:53:09 +0000

    Initial upload

---
+++ config.js
@@ -0,0 +1,16 @@
+﻿var siteConfig = {
+    firebaseUrl: 'https://wormhole-mapper-uat.firebaseio.com/',
+    serverLoginUrl: 'https://fathomless-thicket-9889.herokuapp.com/login',
+    ssoHost: 'https://sisilogin.testeveonline.com',
+
+    maintenanceServerUrl: 'http://shwc-maint-uat.azurewebsites.net/',
+
+    testEnvironment: true,
+    type: 'uat',
+    redirectUri: 'http://holocene.github.io/shdwc-route-finder-uat/#/login-return',
+    clientId: '4f25b205bcb84aaf9c9b5d20d3a57156',
+    devCode: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2IjowLCJkIjp7InVpZCI6IjkzNTYwMzM2MyIsImNoYXJhY3Rlck5hbWUiOiJPbGl4aWEgQ2FzdGl0YXRpcyJ9LCJpYXQiOjE0MTQzMjQ1MDN9.Tmib3A4110wI3LjmJkrY41dn6Ip0ksYRa80StXNpn7M",
+
+    homeSystemId: 30005008
+
+}
